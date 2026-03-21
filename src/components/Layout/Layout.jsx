10d50760d86a9e14d235a8ba686bfd8cc3a9f1da
Divyalakshana <divyalakshanar@gmail.com>
2026-03-21 12:51:12 -0700

    added hanburger-menu to the layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import "../Home/Home.css"; // Keep your sidebar styles here
+import { FaBars } from 'react-icons/fa';
+import "../Home/Home.css";
 
 const Layout = () => {
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setSidebarOpen(!isSidebarOpen);
+    };
+
     return (
         <div className="home-layout">
-            <aside className="sidebar">
+            <header className="mobile-header">
+                <button className="hamburger-menu" onClick={toggleSidebar}>
+                    <FaBars />
+                </button>
+            </header>
+
+            <aside className={`sidebar ${isSidebarOpen ? 'is-open' : ''}`}>
                 <NavLink to="/profile" className="profile-section-link">
                     <div className="profile-section">
-                        <img src="https://via.placeholder.com/150" className="avatar" />
+                        <img src="https://via.placeholder.com/150" className="avatar" alt="User Avatar" />
                         <h3>Hi Selena!</h3>
                     </div>
                 </NavLink>
@@ -20,7 +33,6 @@ const Layout = () => {
                 </nav>
             </aside>
 
-            {/* This is where Home or Profile content will be injected */}
             <main className="content-area">
                 <Outlet />
             </main>
